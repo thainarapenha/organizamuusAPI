72a304127919ab251c699ef5e43e7db8c0329d5e
thainarapenha <thainarapenha.contato@gmail.com>
2026-09-01 18:21:59 -0300

    chore: configure initial API project

---
+++ src/main/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+export const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Olá mundo!",
+    status: "success"        
+  });
+});
